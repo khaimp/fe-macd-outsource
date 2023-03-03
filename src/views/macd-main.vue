afe4b58b9f287e09ffc2b036c003df0f2f99a093
khaimp <khaihust98@gmail.com>
2023-03-03 23:16:40 +0700

    deploy

--- src/views/macd-main.vue
+++ src/views/macd-main.vue
@@ -18,7 +18,7 @@
             role="img"
             aria-hidden="true"
             class="v-icon__svg"
-            :class="{ 'color-red': checkFavorite(item) }"
+            :class="{ 'color-favorite': checkFavorite(item) }"
           >
             <path
               d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"
@@ -364,7 +364,7 @@ export default {
 .color-orange {
   color: #ff9800;
 }
-.color-red {
+.color-favorite {
   color: #ff9800;
 }
 </style>
